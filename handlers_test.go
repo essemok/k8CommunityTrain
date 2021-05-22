a06c199d8d36112152c73cc1cacbd4b6762eefb8
shyrops <beee.semok@gmail.com>
2021-05-22 21:20:48 +0300

    Added logger and test for it

--- handlers_test.go
+++ handlers_test.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"github.com/Sirupsen/logrus"
 	"github.com/takama/router"
 	"io/ioutil"
 	"net/http"
@@ -8,6 +11,14 @@ import (
 	"strings"
 	"testing"
 )
+
+//buffer is a special variable for storing log message
+var buffer bytes.Buffer
+
+func init() {
+	log.Out = &buffer
+	log.Formatter = &logrus.JSONFormatter{}
+}
 
 func TestHandler(t *testing.T) {
 	r := router.New()
@@ -35,5 +46,33 @@ func TestHandler(t *testing.T) {
 		t.Fatalf("Wrong greeting '%s', expected '%s'",
 			testingGreeting, expectedGreeting,
 		)
+	}
+}
+
+func TestLogger(t *testing.T) {
+	r := router.New()
+	r.Logger = logger
+
+	ts := httptest.NewServer(r)
+	defer ts.Close()
+
+	_, err := http.Get(ts.URL + "/")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	formated := struct {
+		Level string `json:"level"`
+		Msg   string `json:"msg"`
+		Time  string `json:"time"`
+	}{}
+	err = json.NewDecoder(&buffer).Decode(&formated)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	msgParts := strings.Split(formated.Msg, " ")
+	if len(msgParts) != 3 {
+		t.Fatalf("Wrong message was loaded: %s", formated.Msg)
 	}
 }
