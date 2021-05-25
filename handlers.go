3c5dac0ad0199b6c112843d7040bbe714601b7a5
shyrops <beee.semok@gmail.com>
2021-05-25 19:09:16 +0300

    Added version information to answer

--- handlers.go
+++ handlers.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"fmt"
+	"github.com/essemok/k8CommunityTrain/version"
 	"github.com/takama/router"
 )
 
 func home(c *router.Control) {
-	fmt.Fprintf(c.Writer, "Processing URL %s...", c.Request.URL.Path)
+	fmt.Fprintf(
+		c.Writer,
+		"Repo %s, Commit %s, Version %s, URL %s...",
+		version.REPO, version.COMMIT, version.RELEASE, c.Request.URL.Path)
 }
 
 func logger(c *router.Control) {
